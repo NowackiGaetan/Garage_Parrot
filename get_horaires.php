1df252d212374ea92d6b54dbc65d8c830fe22062
Gaëtan Nowacki <nowacki.gaetan@hotmail.com>
2023-11-19 09:43:21 +0100

    correction du fichier get_horaires.php

--- get_horaires.php
+++ get_horaires.php
@@ -1,37 +1,33 @@
 <?php
 require('actions/database.php');
 
+$jour_semaine = $_POST['jour_semaine'];
+$horaire_ouverture_matin = htmlspecialchars($_POST['horaire_ouverture_matin']);
+$horaire_fermeture_matin = htmlspecialchars($_POST['horaire_fermeture_matin']);
+$horaire_ouverture_aprem = htmlspecialchars($_POST['horaire_ouverture_aprem']);
+$horaire_fermeture_aprem = htmlspecialchars($_POST['horaire_fermeture_aprem']);
 
-if (isset($_POST['post-coms'])) {
-
-    $jour_semaine = $_POST['jour_semaine'];
-    $horaire_ouverture_matin = htmlspecialchars($_POST['horaire_ouverture_matin']);
-    $horaire_fermeture_matin = htmlspecialchars($_POST['horaire_fermeture_matin']);
-    $horaire_ouverture_aprem = htmlspecialchars($_POST['horaire_ouverture_aprem']);
-    $horaire_fermeture_aprem = htmlspecialchars($_POST['horaire_fermeture_aprem']);
-
-    $sql = "UPDATE horaires_garage SET
+$sql = "UPDATE horaires_garage SET
             horaire_ouverture_matin = :horaire_ouverture_matin,
             horaire_fermeture_matin = :horaire_fermeture_matin,
             horaire_ouverture_aprem = :horaire_ouverture_aprem,
             horaire_fermeture_aprem = :horaire_fermeture_aprem
             WHERE jour_semaine = :jour_semaine";
 
-    $stmt = $pdo->prepare($sql);
+$stmt = $pdo->prepare($sql);
 
-    $stmt->bindParam(':horaire_ouverture_matin', $horaire_ouverture_matin, PDO::PARAM_STR);
-    $stmt->bindParam(':horaire_fermeture_matin', $horaire_fermeture_matin, PDO::PARAM_STR);
-    $stmt->bindParam(':horaire_ouverture_aprem', $horaire_ouverture_aprem, PDO::PARAM_STR);
-    $stmt->bindParam(':horaire_fermeture_aprem', $horaire_fermeture_aprem, PDO::PARAM_STR);
-    $stmt->bindParam(':jour_semaine', $jour_semaine, PDO::PARAM_STR);
+$stmt->bindParam(':horaire_ouverture_matin', $horaire_ouverture_matin, PDO::PARAM_STR);
+$stmt->bindParam(':horaire_fermeture_matin', $horaire_fermeture_matin, PDO::PARAM_STR);
+$stmt->bindParam(':horaire_ouverture_aprem', $horaire_ouverture_aprem, PDO::PARAM_STR);
+$stmt->bindParam(':horaire_fermeture_aprem', $horaire_fermeture_aprem, PDO::PARAM_STR);
+$stmt->bindParam(':jour_semaine', $jour_semaine, PDO::PARAM_STR);
 
-    try {
-        if ($stmt->execute()) {
-            echo "Horaires mis à jour avec succès !";
-        } else {
-            echo "Erreur lors de la mise à jour des horaires.";
-        }
-    } catch (PDOException $e) {
-        echo "Erreur lors de la mise à jour des horaires : " . $e->getMessage();
+try {
+    if ($stmt->execute()) {
+        echo "Horaires mis à jour avec succès !";
+    } else {
+        echo "Erreur lors de la mise à jour des horaires.";
     }
+} catch (PDOException $e) {
+    echo "Erreur lors de la mise à jour des horaires : " . $e->getMessage();
 }
